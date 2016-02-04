2b0b2d2e70ae0b26a6af89cf3268b88ba6eb9c3c
Michail Sandakov <michail.sandakov@eltex.loc>
2016-02-04 15:12:49 +0600

    Add simple working with strings

--- hotkey_text_changer.py
+++ hotkey_text_changer.py
@@ -3,6 +3,13 @@
 
 from Tkinter import *
 
+# There is a test string function
+def string_function(string_in):
+	result_string = ''
+	for char_in in string_in:
+		result_string = result_string + char_in + '.'
+	return result_string
+
 class MainWindow:
 	def __init__(self):
 		self.root = Tk()
@@ -16,18 +23,25 @@ class MainWindow:
 		listbox.insert(END, "somethig if - s")
 
 		# init hotkeys 
-		self.root.bind_all("<q>", self.create_hint)
+		self.root.bind_all("<q>", self.hotkey_reaction)
 
 		self.root.mainloop()
 
 	def hotkey_reaction(self, event):
-		pass
+		result_str = ''
+		try:
+			geted_str  = self.root.clipboard_get()
+			result_str = string_function(geted_str)
+			self.root.clipboard_clear()
+			self.root.clipboard_append(result_str)
+		except:
+			result_str = 'Nothing to show!'
+		self.create_hint(result_str)
 
-	def create_hint(self, event):
-		self.count += 1
-		t=Hint(self.root)
-		t.wm_title("Window %s" % self.count)
-		t.hint_lifecycle()
+	def create_hint(self, out_str):
+		hint_to_show = Hint(self.root)
+		hint_to_show.set_out_string(out_str)
+		hint_to_show.hint_lifecycle()
 
 
 class Hint(Toplevel):
@@ -35,12 +49,17 @@ class Hint(Toplevel):
 	def __init__(self, *args, **kwargs):
 		Toplevel.__init__(self, *args, **kwargs)
 		self.attributes("-alpha", 0.0)
+		self.hint_textbox = Text(self)
+		self.hint_textbox.pack()
+
+	def set_out_string(self, out_string):
+		self.hint_textbox.insert(INSERT, out_string)
 
 	def hint_lifecycle(self):
 		self.fade_in()
 		self.after(3000, self.fade_out)
 
-	def fade_in(self):		
+	def fade_in(self):
 		alpha = self.attributes("-alpha")
 		if alpha == 1:
 			alpha = 0
