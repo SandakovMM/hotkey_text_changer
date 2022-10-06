dbd14b098033e087625df6768f6c71845b6a2629
Mikhail Sandakov <msandakov@plesk.com>
2022-10-06 07:33:03 +0300

    Presho commands

--- cli_text_changer.py
+++ cli_text_changer.py
@@ -8,9 +8,15 @@ import function_loader
 from changers import *
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Clie version of text_changer.')
+    func_loader = function_loader.FunctionStorage()
+    commands_description = 'Can be choosed one of:'
+    for command in func_loader.get_commands_visable_data():
+        commands_description += "\n\t" + command
+
+    parser = argparse.ArgumentParser(description='Clie version of text_changer.',
+        formatter_class=argparse.RawDescriptionHelpFormatter, epilog=commands_description)
     parser.add_argument('-c', '--command', dest='command',
-                        help='command for text treatment')
+                        help='Command for text treatment. ')
     args = parser.parse_args()
 
     if 3 <= sys.version_info.major:
@@ -18,6 +24,5 @@ if __name__ == "__main__":
     else:
         geted_str = raw_input()
 
-    func_loader = function_loader.FunctionStorage()
     function = func_loader.find_command_by_key(args.command)
     print (function(geted_str))
